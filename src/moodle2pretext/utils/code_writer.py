4ba43646ecb4ed2c99fa42e69bf83b75f2bf5885
Haris Skiadas <cskiadas@gmail.com>
2024-08-23 15:53:28 -0400

    Handle full python programs as student inputs by putting them as strings and executing. Close #5

--- src/moodle2pretext/utils/code_writer.py
+++ src/moodle2pretext/utils/code_writer.py
@@ -10,6 +10,7 @@ class CodeWriter():
   def __init__(self) -> None:
     self.env = Environment(loader=PackageLoader("moodle2pretext.utils"))
     self.env.filters['indent'] = indent
+    self.env.tests['is_python_program'] = is_python_program
     self.codeRunnerInput = self.env.get_template("codeRunner-input.py.jinja")
     self.codeRunnerTests = self.env.get_template("codeRunner-tests.py.jinja")
 
@@ -26,3 +27,9 @@ def indent(value, spaces: int|str = 4, includeFirstLine=False):
   if includeFirstLine:
     return spaces + result
   return result
+
+def is_python_program(value):
+  if isinstance(value, CodeRunnerQuestion):
+    return value.testCases[0].testCode.strip() == ""
+  else:
+    raise RuntimeError("Can only use 'is_python_program' on coderunner questions.")
